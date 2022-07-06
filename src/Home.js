a841623380741fc9e47dccd736d894e2a5f769dc
Vedant Jain <thevedantjain@gmail.com>
2022-07-06 22:38:00 +0530

    copied from texts

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
 import './Home.css';
-import Hero from './Components/Hero/Hero';
-import Projects from './Components/Projects/Projects';
-import Testimonial from './Components/Testimonials/Testimonial';
-import Contact from './Components/Contact/Contact';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
 
@@ -14,10 +11,18 @@ class Home extends Component {
     render() {
         return (
             <div className = "Home">
-                <Hero/>
-                <Projects/>
-                <Testimonial/>
-                <Contact/>
+                <span id="time">11:26 PM</span>
+                <h2>hi, I'm Vedant. I am a product designer, based in <span id="emoji">🇮🇳</span></h2>
+                <p>I like designing super simple interfaces</p>
+                <p>I care about accessibility and privacy <span id="emoji">👀</span>. I am always looking for new human centric ideas on the web.</p>
+                <p id="emoji">🙌</p>
+                <p>I like to explore digital interactions, iterating towards simplicity, staring at fonts, and building mechanical keyboards.</p>
+                <span id="time">GO:OD AM</span>
+                <p>See what I’m doing <Link style={{padding: 0}} to="/now">now</Link></p>
+                <span id="time">12:08 AM</span>
+                {/* <p>I also have a <Link style={{padding: 0}} to="/blog">blog</Link>, but I'm pretty new to this :P</p> */}
+                <p>I also have some case studies on <a href="mailto://medium.com/@VedantJ">medium</a></p>
+                <p>get in touch –  <a href="mailto://hello@vedantja.in">hello@vedantja.in</a> or <a href="https://www.twitter.com/vdntjn">twitter</a></p>
             </div>
         )
     }
